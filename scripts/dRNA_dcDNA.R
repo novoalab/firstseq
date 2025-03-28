8ff31efd91f5be05287def6ecc9bcdd14b7d1f52
obegik <oguzhanbegik@gmail.com>
2025-03-28 17:14:25 +0100

    Updated

---
+++ scripts/dRNA_dcDNA.R
@@ -0,0 +1,132 @@
+#!/usr/bin/env Rscript
+
+# ---------------------------
+# Nano-FIRSTseq: dRNA vs dcDNA Comparison
+# CLI Executable R Script
+# Author: Oguzhan Begik (modified for CLI by Code Copilot)
+# ---------------------------
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 3 || any(args %in% c("--help", "-h"))) {
+  cat("Usage: ./1_dRNA_dcDNA.R <dRNA_stats.tsv> <dcDNA_stats.tsv> <mod_positions.tsv>\n")
+  cat("Example: ./1_dRNA_dcDNA.R data/stats/dRNA_YeastTotalRNA_tRNA.STATS data/stats/dcDNA_Maxima.STATS data/references/yeast_mod_positions.tsv\n")
+  quit(save = "no", status = 1)
+}
+
+dRNA_input     <- args[1]
+dcDNA_input    <- args[2]
+mod_file       <- args[3]
+
+# Load required libraries
+suppressPackageStartupMessages({
+  library(reshape2)
+  library(EnvStats)
+  library(ggplot2)
+  library(stringr)
+  library(dplyr)
+  library(tidyr)
+  library(viridis)
+  library(ggbeeswarm)
+})
+
+# Output dir
+output_dir <- "figures"
+dir.create(output_dir, showWarnings = FALSE)
+
+# Define colors
+dRNA_col  <- "#E88D67"
+dcDNA_col <- "#006989"
+
+# Load mod positions
+mod_positions <- read.delim(mod_file)
+
+order_mods <- c("A", "C", "G", "T", "Am", "i6A", "m66A", "t6A", "ac4C", "Cm", 
+                "m5C", "Gm", "m2G", "m7G", "m5U", "Um", "Y", "m1A", "I", "m3C", 
+                "m1G", "m22G", "m1acp3Y", "m3U")
+
+# -- Processing function
+process_func <- function(stats_file_path, method) {
+  stats_file <- read.delim(stats_file_path, sep = "")
+  stats_file <- stats_file %>%
+    filter(pos > 20) %>%
+    mutate(
+      mismatch = rowSums(select(., A, T, C, G), na.rm = TRUE) - .[[.$ref_nuc[1]]],
+      mis_freq = round(mismatch / coverage, 3),
+      uniq_coord = paste(chr, pos, sep = "_"),
+      Method = method
+    )
+  merged <- merge(stats_file, mod_positions, 
+                  by.x = c("chr", "pos"), by.y = c("Reference", "Position"), 
+                  all = TRUE) %>%
+    mutate(
+      Unique = replace_na(Unique, 'Unm'),
+      Mod = replace_na(Mod, 'Unm'),
+      Mod = if_else(Mod == "Unm", ref_nuc, Mod)
+    )
+  return(merged)
+}
+
+# -- Aggregation function
+clean_mis_input_agg <- function(data) {
+  data <- data %>% filter(ref_nuc %in% c("A", "T", "C", "G")) %>%
+    mutate(sum = A + T + C + G,
+           A = A / sum, T = T / sum, C = C / sum, G = G / sum)
+  data_count <- count(data, Mod)
+  data_avg <- aggregate(data[, c("A", "T", "C", "G")],
+                        by = list(data$Mod, data$ref_nuc),
+                        FUN = mean, na.rm = TRUE)
+  data_melted <- melt(data_avg, id.vars = c("Group.1", "Group.2"))
+  final_data <- merge(data_melted, data_count, by.x = "Group.1", by.y = "Mod")
+  return(final_data)
+}
+
+# -- Plot barplot
+mismatch_profile_barplot_facet <- function(data, label) {
+  # Assign color: gray for ref base, custom for others
+  base_colors <- c("A" = "#1fab89", "T" = "#eb4d55", "C" = "#1e56a0", "G" = "#f0cf85")
+  data$fill_color <- ifelse(data$variable == data$Group.2, "#888888", base_colors[data$variable])
+
+  pdf(file = file.path(output_dir, paste0(label, "_mismatch_profile_barplot.pdf")), 
+      height = 1.5, width = 6)
+
+  print(
+    ggplot(data, aes(x = Group.1, y = value, fill = fill_color)) +
+      geom_bar(stat = 'identity', colour = "black") +
+      scale_fill_identity() +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  )
+
+  dev.off()
+}
+
+
+# -- Plot dotplot
+dotplot_facet <- function(data, label) {
+  data <- data %>% filter(!(Mod %in% c("A", "G", "C", "T", "a", "t", "g", "c", 
+                                       "N", "xU", "mcm5s2U", "ncm5U", "mcm5U", "m1I")))
+  data$Mod <- factor(data$Mod, levels = order_mods)
+  pdf(file = file.path(output_dir, paste0(label, "_Dotplot.pdf")), height = 3, width = 15)
+  print(
+    ggplot(data, aes(x = Method, y = mis_freq, color = Method)) +
+      geom_quasirandom(varwidth = TRUE, alpha = 0.6) +
+      geom_boxplot(width = 0.3, size = 0.5, alpha = 1) +
+      scale_color_manual(values = c(dcDNA_col, dRNA_col)) +
+      facet_wrap(~ Mod, nrow = 1) +
+      theme_classic()
+  )
+  dev.off()
+}
+
+# ---------- RUN PIPELINE ----------
+dRNA.processed <- process_func(dRNA_input,  "dRNA-seq")
+cDNA.processed <- process_func(dcDNA_input, "dcDNA-seq")
+
+dRNA.mis <- clean_mis_input_agg(dRNA.processed)
+cDNA.mis <- clean_mis_input_agg(cDNA.processed)
+
+mismatch_profile_barplot_facet(dRNA.mis, "dRNA-seq")
+mismatch_profile_barplot_facet(cDNA.mis, "dcDNA-seq")
+
+dotplot_facet(rbind(dRNA.processed, cDNA.processed), "Modifications_Mismatch")
